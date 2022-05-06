7ad7692327246e0925d7928b65728f09b8d8882f
NEETAMOURYA <neeta.mourya@bridgelabz.com>
2022-05-06 13:18:47 +0530

    Added: Helloworld Spring App to display “Hello from BridgeLabz” using STS

---
+++ src/main/java/com/example/firstspringappapplication/FirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstspringappapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Bridgelabz ");
+        SpringApplication.run(FirstSpringAppApplication.class, args);
+    }
+}
